d85a8af90898196e99d043885a8a69fe5e43c6f2
paltom <tom0palka@gmail.com>
2020-04-17 17:59:44 +0200

    replace multiple conditions with cond

--- src/clojure_workshop/1/a03.clj
+++ src/clojure_workshop/1/a03.clj
@@ -1,9 +1,7 @@
 (defn meditate
   [s calmness-level]
   (println "Clojure Meditate v2.0")
-  (if (< calmness-level 5)
-    (str (clojure.string/upper-case s) ", I TELL YA!")
-    (if (<= 5 calmness-level 9)
-      (clojure.string/capitalize s)
-      (if (= calmness-level 10)
-        (clojure.string/reverse s)))))
+  (cond
+    (< calmness-level 5)    (str (clojure.string/upper-case s) ", I TELL YA!")
+    (<= 5 calmness-level 9) (clojure.string/capitalize s)
+    (= calmness-level 10)   (clojure.string/reverse s)))
