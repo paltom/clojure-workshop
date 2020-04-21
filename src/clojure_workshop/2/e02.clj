a556d1532549faf07dd8cc962534cac3a661089a
paltom <tom0palka@gmail.com>
2020-04-21 16:31:06 +0200

    Collections exercise - maps

---
+++ src/clojure_workshop/2/e02.clj
@@ -0,0 +1,34 @@
+; Maps
+(def favorite-fruit
+  {:name "Kiwi", :color "Green", :kcal_per_100g 61, :distinguish-mark "Hairy"})
+
+(get favorite-fruit :name)
+(get favorite-fruit :color)
+(get favorite-fruit :taste)
+(get favorite-fruit :taste "Very good 8/10")
+(get favorite-fruit :kcal_per_100g 0)
+
+(favorite-fruit :color)
+(:color favorite-fruit)
+(:shape favorite-fruit "egg-like")
+(assoc favorite-fruit :shape "egg-like")
+favorite-fruit
+
+(assoc favorite-fruit :color "Brown")
+favorite-fruit
+
+(assoc favorite-fruit :yearly-production-in-tonnes
+       {:china 2025000
+        :italy 541000
+        :new_zealand 412000
+        :iran 311000
+        :chile 225000})
+
+(assoc favorite-fruit :kcal_per_100g (- (:kcal_per_100g favorite-fruit) 1))
+(update favorite-fruit :kcal_per_100g dec)
+(update favorite-fruit :kcal_per_100g - 10)
+
+(dissoc favorite-fruit :distinguish-mark)
+(dissoc favorite-fruit :kcal_per_100g :color)
+
+; Sets
