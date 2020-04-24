af41ec309010dd24eb8785b502d15f7efa9aa22d
paltom <tom0palka@gmail.com>
2020-04-24 15:45:29 +0200

    Exercise 3.02 - Associative destructuring

---
+++ src/clojure_workshop/3/e02.clj
@@ -0,0 +1,40 @@
+(def mapjet-booking
+  {:id 8773
+   :customer-name "Alice Smith"
+   :catering-notes "Vegetarian on Sundays"
+   :flights [{:from {:lat 48.9615
+                     :lon 2.4372
+                     :name "Paris Le Bourget Airport"}   
+              :to {:lat 37.742
+                   :lon -25.6976
+                   :name "Ponta Delgada Airport"}}
+             {:from {:lat 37.742
+                     :lon -25.6976
+                     :name "Ponta Delgada Airport"}
+              :to {:lat 48.9615
+                   :lon 2.437
+                   :name "Paris Le Bourget Airport"}}]})
+
+(let [{:keys [customer-name flights]} mapjet-booking]                
+  (println (str customer-name " booked " (count flights) " flights.")))
+
+(defn print-mapjet-flight
+  [flight]
+  (let [{:keys [from to]} flight
+        {lat1 :lat lon1 :lon} from
+        {lat2 :lat lon2 :lon} to]
+    (println (str "Flying from:" " Lat " lat1 " Lon " lon1 "\n"
+                  "Flying to:" " Lat " lat2 " Lon " lon2))))
+
+(print-mapjet-flight (first (:flights mapjet-booking)))
+
+(defn print-mapjet-booking
+  [booking]
+  (let [{:keys [customer-name flights]} booking]
+    (println (str customer-name " booked " (count flights) " flights."))
+    (let [[flight1 flight2 flight3] flights]
+      (when flight1 (print-mapjet-flight flight1))
+      (when flight2 (print-mapjet-flight flight2))
+      (when flight3 (print-mapjet-flight flight3)))))
+
+(print-mapjet-booking mapjet-booking)
